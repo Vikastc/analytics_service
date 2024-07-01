25c7f9dc2c6ce46e2a81a6a2fa3ebf561ba5599b
vikastc <vikas.tc@dhiway.com>
2024-07-01 11:14:58 +0530

    fix: Refactored code
    Signed-off-by: vikastc <vikas.tc@dhiway.com>

--- src/controller/event_controller.ts
+++ src/controller/event_controller.ts
@@ -3,27 +3,36 @@ import express from "express";
 import axios from "axios";
 import client from "./redisClient";
 
+export async function getOrSetCache(key: string, cb: any) {
+    const dataObj = await client.get(key);
+
+    if (dataObj) {
+        console.log("used cache");
+        const parsedData = JSON.parse(dataObj);
+
+        return parsedData;
+    }
+
+    const freshData = await cb();
+
+    // Sets a key value pair as photos: "{}" with expiration of 5 seconds
+    await client.setEx(key, 5, JSON.stringify(freshData));
+    console.log("set cache");
+    return freshData;
+}
+
 export async function setAndGetFromRedis(
     req: express.Request,
     res: express.Response
 ) {
     try {
         // Checks if there is a key "photos" in redis
-        const photos = await client.get("photos");
+        const photos = await getOrSetCache("photos", async () => {
+            const response = await axios.get("https://picsum.photos/v2/list");
+            return response.data;
+        });
 
-        if (photos) {
-            console.log("used cache");
-            const parsedData = JSON.parse(photos);
-            return res.json(parsedData);
-        }
-
-        const { data } = await axios.get("https://picsum.photos/v2/list");
-
-        // Sets a key value pair as photos: "{}" with expiration of 5 seconds
-        await client.setEx("photos", 5, JSON.stringify(data));
-        console.log("set cache");
-
-        return res.json(data);
+        return res.json(photos);
     } catch (error) {
         console.error("Error:", error);
         res.status(500).send("Internal Server Error");
