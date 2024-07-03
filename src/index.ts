27a32f15890708c44d8c10730f260ed8c31c2e38
vikastc <vikas.tc@dhiway.com>
2024-07-03 16:48:20 +0530

    fix: Add cluster
    Signed-off-by: vikastc <vikas.tc@dhiway.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import fs from "fs";
 import swaggerUi from "swagger-ui-express";
+import cluster from "node:cluster";
+import os from "node:os";
 import { createConnection } from "typeorm";
 
 import { dbConfig } from "./dbconfig";
@@ -15,47 +17,64 @@ export const { PORT } = process.env;
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(express.json());
 
-const redisRouter = express.Router({ mergeParams: true });
+const numCPUs = os.cpus().length;
 
-redisRouter.get("/", async (req, res) => {
-    return await setAndGetFromRedis(req, res);
-});
+if (cluster.isMaster) {
+    console.log(`Primary ${process.pid} is running`);
 
-const openApiDocumentation = JSON.parse(
-    fs.readFileSync("./apis.json").toString()
-);
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
-app.use("/api/v1/redis", redisRouter);
-
-app.post("/api/v1/send-event", async (req, res) => {
-    return await manageEvent(req, res);
-});
-
-app.post("/api/v1/query-metrics", async (req, res) => {
-    return await queryMetrics(req, res);
-});
-
-// All other routes to React App
-app.get("/*", async (req, res) => {
-    return res.json({
-        message: "check https://docs.dhiway.com/api for details of the APIs",
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
     });
-});
+} else {
+    const redisRouter = express.Router({ mergeParams: true });
 
-async function main() {
-    try {
-        // Connect the Redis client
-        await connectRedis();
+    redisRouter.get("/", async (req, res) => {
+        return await setAndGetFromRedis(req, res);
+    });
 
-        await createConnection(dbConfig);
-    } catch (error) {
-        console.log("error: ", error);
+    const openApiDocumentation = JSON.parse(
+        fs.readFileSync("./apis.json").toString()
+    );
+
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+    app.use("/api/v1/redis", redisRouter);
+
+    app.post("/api/v1/send-event", async (req, res) => {
+        return await manageEvent(req, res);
+    });
+
+    app.post("/api/v1/query-metrics", async (req, res) => {
+        return await queryMetrics(req, res);
+    });
+
+    // All other routes to React App
+    app.get("/*", async (req, res) => {
+        return res.json({
+            message:
+                "check https://docs.dhiway.com/api for details of the APIs",
+        });
+    });
+
+    async function main() {
+        try {
+            // Connect the Redis client
+            // await connectRedis();
+
+            await createConnection(dbConfig);
+        } catch (error) {
+            console.log("error: ", error);
+        }
+
+        app.listen(PORT, () => {
+            console.log(
+                `Dhiway gateway is running at http://localhost:${PORT}`
+            );
+        });
     }
 
-    app.listen(PORT, () => {
-        console.log(`Dhiway gateway is running at http://localhost:${PORT}`);
-    });
+    main().catch((e) => console.log(e));
 }
-
-main().catch((e) => console.log(e));
